007c60ab38bfcba47d99cbd54b2b245f24ca1914
juan herrera <juandavidherrera@gmail.com>
2024-11-06 14:09:29 -0500

    commit2

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const colors = require('colors'); 
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
+
 app.get('/', (req, res) => res.redirect('/login'));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
-app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
-app.get('/welcome', (req, res) => res.sendFile(path.join(__dirname, 'views', 'welcome.html')));
 
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.get('/login', (req, res) => {
+    console.log("Accediendo a la página de login".green); 
+    res.render('login');
+});
+
+
+app.get('/register', (req, res) => {
+    console.log("Accediendo a la página de registro".yellow);
+    res.render('register');
+});
+
+
+app.get('/welcome', (req, res) => {
+    const userName = "Usuario"; 
+    console.log("Accediendo a la página de bienvenida".cyan);
+    res.render('welcome', { userName });
+});
+
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'.blue));
